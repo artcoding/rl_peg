f2914b0e192aa6ddd8401703b994e882026989a9
artcoding <1243401+artcoding@users.noreply.github.com>
2022-06-08 08:44:00 -0400

    Add non-trivial mirror tests

--- src/state.py
+++ src/state.py
@@ -12,27 +12,29 @@ class State:
         # for row in range(1, rows):
         #     for pos in
 
-    def __eq__(self, other):
+    def from_value(self, value: int) -> State:
+        return State(value=value, rows=self.rows)
+
+    def __eq__(self, other: State) -> bool:
         """Overrides the default implementation"""
         if isinstance(other, State):
             return self.value == other.value and self.rows == self.rows
         return False
 
-    # This function swaps bit at positions p1 and p2 in an integer n
-    def swap_bits(self, p1: int, p2: int):
+    def swap_bits(self, p1: int, p2: int) -> None:
+        """
+        Swap bits at positions p1 and p2 in self.value
+        """
 
         # Move p1'th to rightmost side
         bit1 = (self.value >> p1) & 1
-
         # Move p2'th to rightmost side
         bit2 = (self.value >> p2) & 1
-
         if bit1 == bit2:
             return
 
         # XOR the two bits
         x = (bit1 ^ bit2)
-
         # Put the xor bit back to their original positions
         x = (x << p1) | (x << p2)
 
@@ -44,6 +46,9 @@ class State:
         return self
 
     def mirror(self) -> State:
+        """
+        Return image of state reflected in vertical mirror.
+        """
         other = copy(self)
         for row in range(1, self.rows):
             start = (1 + row) * row // 2
--- tests/test_state.py
+++ tests/test_state.py
@@ -5,3 +5,12 @@ def test_mirror():
     new_state = state.mirror()
     assert new_state == state
 
+    # Mirror 101100 (1 + 4 + 8 = 13) into 110001 (1 + 2 + 32 = 35)
+    state = src.State(value=13, rows=3)
+    new_state = state.mirror()
+    print(new_state.value)
+    assert new_state == state.from_value(value=35)
+
+    new_state = new_state.mirror()
+    assert new_state == state
+
