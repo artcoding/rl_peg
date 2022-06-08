ad06ffb1393d8607317f5ca01e58bbb38273e314
artcoding <1243401+artcoding@users.noreply.github.com>
2022-06-08 08:53:31 -0400

    One more test of mirror()

--- tests/test_state.py
+++ tests/test_state.py
@@ -8,9 +8,17 @@ def test_mirror():
     # Mirror 101100 (1 + 4 + 8 = 13) into 110001 (1 + 2 + 32 = 35)
     state = src.State(value=13, rows=3)
     new_state = state.mirror()
-    print(new_state.value)
     assert new_state == state.from_value(value=35)
 
+    # Mirror bakc to the original state
     new_state = new_state.mirror()
     assert new_state == state
 
+    # Mirror 001101 (4 + 8 + 32 = 44) into 010101 (2 + 8 + 32 = 42)
+    state = src.State(value=44, rows=3)
+    new_state = state.mirror()
+    assert new_state == state.from_value(value=42)
+
+    # Mirror bakc to the original state
+    new_state = new_state.mirror()
+    assert new_state == state
