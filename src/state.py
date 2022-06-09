b75567fd9ecadd02a42853a47120aeb16bf94940
artcoding <1243401+artcoding@users.noreply.github.com>
2022-06-09 16:01:52 -0400

    Various utilities for State class.

--- src/state.py
+++ src/state.py
@@ -2,9 +2,11 @@
 from __future__ import annotations
 from copy import copy
 
+from src.utils import calc_space, space_moves
+
 
 class State:
-    def __init__(self, value: int, rows: int=5):
+    def __init__(self, value: int, rows: int = 5):
         self.value = value
         self.rows = rows
 
@@ -23,7 +25,22 @@ class State:
     def __repr__(self) -> str:
         return f"value = {self.value}; rows = {self.rows}"
 
-    def swap_bits(self, p1: int, p2: int) -> None:
+    def spaces(self) -> int:
+        return (1 + self.rows) * self.rows // 2
+
+    def is_empty(self, space: int) -> bool:
+        return self.value & (1 << space) == 0
+
+    def set_space(self, space: int) -> None:
+        self.value |= (1 << space)
+
+    def clear_space(self, space: int) -> None:
+        self.value &= ~(1 << space)
+
+    def copy(self) -> State:
+        return State(self.value, self.rows)
+
+    def _swap_bits(self, p1: int, p2: int) -> None:
         """
         Swap bits at positions p1 and p2 in self.value
         """
@@ -46,7 +63,7 @@ class State:
 
     def rotate(self) -> State:
         """
-        Return image of state rotated clockwise\.
+        Return image of state rotated clockwise.
         """
         rotated = self.from_value(0)
 
@@ -57,12 +74,10 @@ class State:
 
             for new_row in range(self.rows - 1 - row, self.rows):
                 # bit position in rotated value
-                new_bit_pos = (1 + new_row) * new_row // 2 + new_row_pos
+                new_bit_pos = calc_space(new_row, new_row_pos)
 
-                # Read bit from bit_pos in self.value
-                bit = self.value & (1 << bit_pos)
                 # Insert bit to new_bit_pos in rotated.value
-                if bit != 0:
+                if not self.is_empty(bit_pos):
                     rotated.value = rotated.value | (1 << new_bit_pos)
                 bit_pos += 1
 
@@ -78,7 +93,7 @@ class State:
             count = row // 2 + 1
             for c in range(count):
                 # switch bits in positions pos and stop - pos
-                other.swap_bits(start + c, start + count - c)
+                other._swap_bits(start + c, start + count - c)
 
         return other
 
@@ -92,3 +107,23 @@ class State:
 
         res = {self, mirror, rotated, rotated_mirror, rotated.rotate(), rotated_mirror.rotate()}
         return res
+
+
+def find_moves(state: State) -> {State}:
+    space_moves_ = space_moves(state.rows)
+    new_states = set()
+
+    for space in range(state.spaces()):
+        if state.is_empty(space):
+            continue
+
+        for space_move in space_moves_[space]:
+            kill, move_to = space_move
+            if not state.is_empty(kill) and state.is_empty(move_to):
+                new_state = state.copy()
+                new_state.clear_space(space)
+                new_state.clear_space(kill)
+                new_state.set_space(move_to)
+                new_states.add(new_state)
+
+    return new_states
