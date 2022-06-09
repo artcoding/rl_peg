4b7c69de06b2b0f86397a166043e0d35b38f2c9b
artcoding <1243401+artcoding@users.noreply.github.com>
2022-06-09 16:10:11 -0400

    Calculate all possible states to switch to.

--- src/q_learn/main.py
+++ src/q_learn/main.py
@@ -1,18 +1,20 @@
-#
-from src import State
+# Entry point for the Q-learning algorithm
 
-
-def find_moves(state: State) -> {State}:
-    pass
+from src import State, find_moves
+from src import space_moves
 
 
 def learn(rows: int = 5):
     states = {}
 
+    print(space_moves(rows))
+
     n_holes = (1 + rows) * rows // 2
     current_state = State(value=(1 << n_holes) - 2 - (1 << 3), rows=rows)
 
-    print(current_state)
+    print(f"current state = {current_state.value}")
+    print(f"moves = {find_moves(current_state)}")
+    print(f"new states = {[bin(s.value) for s in find_moves(current_state)]}")
 
     equivs = current_state.all_equivalent()
     print(equivs)
