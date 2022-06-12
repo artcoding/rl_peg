5af8e4cb7f16c9f99d6b24d3ccab46c377da8a19
artcoding <1243401+artcoding@users.noreply.github.com>
2022-06-12 15:02:15 -0400

    Minor change to __repr__()

--- src/state.py
+++ src/state.py
@@ -22,7 +22,7 @@ class State:
         return self.value
 
     def __repr__(self) -> str:
-        return f"value = {self.value}, {bin(self.value)}"
+        return f"state = {self.value} ({bin(self.value)})"
 
     def spaces(self) -> int:
         return (1 + self.rows) * self.rows // 2
