7c40b740942ed362b38449492703d30afd45631c
artcoding <1243401+artcoding@users.noreply.github.com>
2022-06-12 00:14:19 -0400

    Start building the learning loop.

--- src/q_learn/main.py
+++ src/q_learn/main.py
@@ -1,16 +1,15 @@
 # Entry point for the Q-learning algorithm
 
-from src import State, find_moves
-from src import space_moves
+from src import State, Qclass, find_moves, space_moves
 
 
 def learn(rows: int = 5):
-    states = {}
+    q = Qclass()
 
     print(space_moves(rows))
 
     n_holes = (1 + rows) * rows // 2
-    current_state = State(value=(1 << n_holes) - 2 - (1 << 3))
+    current_state = State(value=(1 << n_holes) - 2)
 
     print(f"current state = {current_state.value}")
     print(f"moves = {find_moves(current_state)}")
@@ -18,7 +17,16 @@ def learn(rows: int = 5):
     equivs = current_state.all_equivalent()
     print(equivs)
 
+    symmetry_map = {}
+
     while True:
+        if current_state not in symmetry_map:
+            all_transforms = current_state.all_equivalent()
+            for state in all_transforms:
+                symmetry_map[state] = current_state
+
+
+
         break
 
 
