6e2ba6424dcbcfd4734fc7c8622aef7798800b9b
artcoding <1243401+artcoding@users.noreply.github.com>
2022-06-13 03:19:11 -0400

    Refactor Qclass

--- src/q_class.py
+++ src/q_class.py
@@ -1,15 +1,19 @@
 import numpy as np
-from src import State, Action
+from src import State, Action, find_moves
+from typing import Optional
 
 
 class Qclass:
 
-    def __init__(self, epsilon: float):
-        self.eps = epsilon
+    def __init__(self, eps: float, alpha: float, lmbda: float):
+        self.eps = eps
+        self.alpha = alpha
+        self.lmbda = lmbda
+
         self.value = {}
         self.eligibility = {}
 
-    def choose_action(self, state: State) -> Action:
+    def choose_action(self, state: State) -> Optional[Action]:
         """
         Epsilon-greedy algorithm to choose next action.
 
@@ -18,6 +22,8 @@ class Qclass:
         """
         actions = self.value[state]
         m = len(actions)
+        if m == 0:
+            return None
         eps_m = self.eps / m
         i_max = np.argmax(actions.values())
 
@@ -28,12 +34,26 @@ class Qclass:
             i = i_max
         else:
             i = m - 1 - int((1 - rand) / eps_m)
-        return actions[i]
+        return list(actions)[i]
 
-    def add(self, state: State, action: Action, delta: float):
+    def reset_eligibility(self):
+        for state, actions in self.eligibility.items():
+            for action in actions:
+                self.eligibility[state][action] = 0
+
+    def add_state(self, state: State, all_space_moves):
         if state not in self.value:
-            self.value[state] = {action: delta}
-        elif action not in self.value[state]:
-            self.value[state][action] = delta
-        else:
-            self.value[state][action] += delta
+            moves = find_moves(state, all_space_moves)
+            self.value[state] = {act: 0.0 for (act, st) in moves}
+            self.eligibility[state] = {act: 0.0 for (act, st) in moves}
+
+    def update(self, delta: float):
+        for state, actions in self.value.items():
+            for action in actions:
+                self.value[state][action] += self.alpha * delta * self.eligibility[state][action]
+                self.eligibility[state][action] *= self.lmbda
+
+    def __call__(self, state, action):
+        if action is None:
+            return 0
+        return self.value[state][action]
