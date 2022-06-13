a1997a008934efc970320ccf0a9fbbd4393196b4
artcoding <1243401+artcoding@users.noreply.github.com>
2022-06-13 00:40:49 -0400

    Implement epsilon-greedy policy

--- src/q_class.py
+++ src/q_class.py
@@ -1,17 +1,39 @@
+import numpy as np
 from src import State, Action
 
 
 class Qclass:
 
-    def __init__(self):
-        self.Q = {}
+    def __init__(self, epsilon: float):
+        self.eps = epsilon
+        self.value = {}
+        self.eligibility = {}
 
-        pass
+    def choose_action(self, state: State) -> Action:
+        """
+        Epsilon-greedy algorithm to choose next action.
+
+        :param state:   Current state
+        :return:        Action to take
+        """
+        actions = self.value[state]
+        m = len(actions)
+        eps_m = self.eps / m
+        i_max = np.argmax(actions.values())
+
+        rand = np.random.uniform()
+        if rand < eps_m * i_max:
+            i = int(rand / eps_m)
+        elif rand < eps_m * i_max + 1 - self.eps:
+            i = i_max
+        else:
+            i = m - 1 - int((1 - rand) / eps_m)
+        return actions[i]
 
     def add(self, state: State, action: Action, delta: float):
-        if state not in self.Q:
-            self.Q[state] = {action: delta}
-        elif action not in self.Q[state]:
-            self.Q[state][action] = delta
+        if state not in self.value:
+            self.value[state] = {action: delta}
+        elif action not in self.value[state]:
+            self.value[state][action] = delta
         else:
-            self.Q[state][action] += delta
+            self.value[state][action] += delta
